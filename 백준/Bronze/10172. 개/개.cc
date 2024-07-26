7c8240e2e611567efe2bf4d9496c81d5d06fd30c
김승현 <109491115+shkim8@users.noreply.github.com>
2024-07-26 16:50:34 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string str;
+    str = "|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|";
+    cout << str;
+    
+    return 0;
+}
